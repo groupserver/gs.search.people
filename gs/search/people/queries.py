e9331e9eb5b43089ca2e7a849ef26077f172cf89
Michael JasonSmith <mpj17@onlinegroups.net>
2012-05-02 11:17:44 +1000

    Adding some globs to the email-address search.

--- gs/search/people/queries.py
+++ gs/search/people/queries.py
@@ -8,7 +8,8 @@ class SearchPeopleQuery(object):
     def find_uids_by_email(self, emailAddress):
         uet = self.userEmailTable
         s = sa.select([uet.c.user_id])
-        s.append_whereclause(uet.c.email.op('ILIKE')(emailAddress))
+        expr = '%%%s%%' % emailAddress
+        s.append_whereclause(uet.c.email.op('ILIKE')(expr))
     
         r = s.execute()
         retval = [x['user_id'] for x in r]
