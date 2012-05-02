aa14ecfce64727cc117a8de2608cb751afefd591
Michael JasonSmith <mpj17@onlinegroups.net>
2012-05-02 16:16:51 +1000

    Do an exact search for the email address (ignorning case).

--- gs/search/people/queries.py
+++ gs/search/people/queries.py
@@ -8,11 +8,12 @@ class SearchPeopleQuery(object):
     def find_uids_by_email(self, emailAddress):
         uet = self.userEmailTable
         s = sa.select([uet.c.user_id])
-        expr = '%%%s%%' % emailAddress
+        # Use an ILIKE so the search is case insensitive.
+        expr = '%s' % emailAddress
         s.append_whereclause(uet.c.email.op('ILIKE')(expr))
     
         r = s.execute()
-        retval = [x['user_id'] for x in r]
-        assert type(retval) == list
+        rv = [x['user_id'] for x in r]
+        retval = (rv and rv[0]) or None
         return retval
 
