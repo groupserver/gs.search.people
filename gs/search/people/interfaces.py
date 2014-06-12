a3b462ec9becf4adcb95d3a4f33efb1b83746a71
Michael JasonSmith <mpj17@onlinegroups.net>
2014-06-11 17:30:46 -0700

    Switching to Unicode literals and absolute import

--- gs/search/people/interfaces.py
+++ gs/search/people/interfaces.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
+from __future__ import absolute_import
 from zope.interface import Interface
 from zope.schema import TextLine
 
 
 class IGSSearchPeople(Interface):
-    email = TextLine(title=u'Email address',
-                        description=u'The email address of the person you '
-                                    u'wish to find.',
+    email = TextLine(title='Email address',
+                        description='The email address of the person you '
+                                    'wish to find.',
                         required=True)
--- gs/search/people/searchpeople.py
+++ gs/search/people/searchpeople.py
@@ -1,4 +1,18 @@
 # -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright © 2013 OnlineGroups.net and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+from __future__ import absolute_import, unicode_literals
 from email.utils import parseaddr
 from zope.cachedescriptors.property import Lazy
 from zope.component import createObject
@@ -6,12 +20,12 @@ from zope.formlib import form
 from Products.Five.browser.pagetemplatefile import ZopeTwoPageTemplateFile
 from gs.content.form.base import SiteForm
 from gs.site.member import SiteMembers
-from interfaces import IGSSearchPeople
-from queries import SearchPeopleQuery
+from .interfaces import IGSSearchPeople
+from .queries import SearchPeopleQuery
 
 
 class SearchPeople(SiteForm):
-    label = u'Search for a site member'
+    label = 'Search for a site member'
     pageTemplateFileName = 'browser/templates/searchpeople.pt'
     template = ZopeTwoPageTemplateFile(pageTemplateFileName)
     form_fields = form.Fields(IGSSearchPeople, render_context=False)
@@ -25,26 +39,24 @@ class SearchPeople(SiteForm):
         retval = SiteMembers(self.context)
         return retval
 
-    @form.action(label=u'Search', failure='handle_search_action_failure')
+    @form.action(label='Search', failure='handle_search_action_failure')
     def handle_search(self, action, data):
 
         email = parseaddr(data['email'])[1]
         userId = self.searchQuery.find_uids_by_email(email)
 
         if userId and (userId in self.siteMembers):
-            self.status = u'Be joyous! You found someone.'
+            self.status = 'Be joyous! You found someone.'
             userInfo = createObject('groupserver.UserFromId',
                                     self.context, userId)
             uri = userInfo.url
             return self.request.RESPONSE.redirect(uri)
         else:
-            self.status = u'Could not find any site-member with the '\
-                u'email address <code class="email">%s</code>.' % email
-        assert type(self.status) == unicode
+            self.status = 'Could not find any site-member with the '\
+                'email address <code class="email">%s</code>.' % email
 
     def handle_search_action_failure(self, action, data, errors):
         if len(errors) == 1:
-            self.status = u'<p>There is an error:</p>'
+            self.status = '<p>There is an error:</p>'
         else:
-            self.status = u'<p>There are errors:</p>'
-        assert type(self.status) == unicode
+            self.status = '<p>There are errors:</p>'
