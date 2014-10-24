c2155f0441f1c4212b7b2db4b1bd4a4dc8b36d9d
Michael JasonSmith <mpj17@onlinegroups.net>
2014-10-24 14:50:12 -0700

    Updating the README and HISTORY

--- docs/HISTORY.rst
+++ docs/HISTORY.rst
@@ -1,3 +1,36 @@
 Changelog
 =========
 
+2.1.0 (2014-10-24)
+------------------
+
+* Adding internationalisation
+* Using GitHub_ as the main code repository, and naming the
+  reStructuredText files as such.
+
+.. _GitHub: https://github.com/groupserver/gs.search.people/
+
+2.0.1 (2014-06-12)
+------------------
+
+* Following the ``gs.content.form.base`` code
+
+2.0.0 (2013-07-23)
+------------------
+
+* Updating to the new UI
+
+1.1.0 (2012-07-23)
+------------------
+
+* Removing an adaptor
+
+1.0.1 (2012-06-22)
+------------------
+
+* Update because of the change to SQLalchemy
+
+1.0.0 (2012-03-02)
+------------------
+
+* Initial version
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-##############################################################################
+############################################################################
 #
-# Copyright © 2013 OnlineGroups.net and Contributors.
+# Copyright © 2012, 2013, 2014 OnlineGroups.net and Contributors.
 # All Rights Reserved.
 #
 # This software is subject to the provisions of the Zope Public License,
@@ -11,7 +11,7 @@
 # WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
 # FOR A PARTICULAR PURPOSE.
 #
-##############################################################################
+############################################################################
 import codecs
 import os
 from setuptools import setup, find_packages
@@ -21,10 +21,12 @@ version = get_version()
 
 with codecs.open('README.rst', encoding='utf-8') as f:
     long_description = f.read()
-with codecs.open(os.path.join("docs", "HISTORY.rst"), encoding='utf-8') as f:
+with codecs.open(os.path.join("docs", "HISTORY.rst"),
+                 encoding='utf-8') as f:
     long_description += '\n' + f.read()
 
-setup(name='gs.search.people',
+setup(
+    name='gs.search.people',
     version=version,
     description="Searching for people on GroupServer.",
     long_description=long_description,
@@ -38,11 +40,11 @@ setup(name='gs.search.people',
         "Operating System :: POSIX :: Linux",
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
-      ],
+    ],
     keywords='site groupserver name',
     author='Michael JasonSmith',
     author_email='mpj17@onlinegroups.net',
-    url='https://source.iopen.net/groupserver/gs.search.people/',
+    url='https://github.com/groupserver/gs.search.people/',
     license='ZPL 2.1',
     packages=find_packages(exclude=['ez_setup']),
     namespace_packages=['gs', 'gs.search', ],
